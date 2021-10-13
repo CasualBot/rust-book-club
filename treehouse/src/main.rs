1b80f3bcd6457e38bdc3b34d11bd88a55efea337
Shawn <sbernard199@gmail.com>
2021-10-12 20:58:54 -0500

    chapter 2 done

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -2,47 +2,75 @@
 
 use std::io::stdin;
 
+#[derive(Debug)]
 struct Visitor {
     name: String,
-    greeting: String,
+    greeting: String, 
+    action: VisitorAction,
+    age: i8
 }
+#[derive(Debug)]
+enum VisitorAction {
+    Accept,
+    AcceptWithNote { note: String },
+    Refuse,
+    Probation,
+} 
 
 impl Visitor {
-    fn new(name: &str, greeting: &str) -> Self {
+    fn new(name: &str, greeting: &str, action: VisitorAction, age: i8) -> Self {
         Self {
             name: name.to_lowercase(),
             greeting: greeting.to_string(),
+            action,
+            age,
         }
     }
 
     fn greet_visitor(&self) {
+        match &self.action {
+            VisitorAction::Accept => println!("We lcome to the treehouse, {}", self.name),
+            VisitorAction::AcceptWithNote { note } => {
+                println!("Welcome to the treehouse, {}", self.name);
+                println!("{}", note);
+                if self.age < 21 {
+                    println!("Do not serve alcohol to {}", &self.name);
+                }
+            },
+            VisitorAction::Probation => println!("{} is now a probationary member", self.name),
+            VisitorAction::Refuse => println!("Do not allow {} in.", self.name),
+        }
         println!("{}", self.greeting);
     }
 }
 
 fn main() {
-    println!("Hello, what's your name?\n");
-    let name = what_is_your_name();
-    let mut allow_them_in = false;
-
-    let visitor_list = [
-        Visitor::new("Shawn", "Hello Shawn, enjoy your treehouse!"),
-        Visitor::new("Bert", "Hey Bernt Krishna, I like your stand up."),
-        Visitor::new("Tom", "Tom, get out.")
+    let mut visitor_list = vec![
+        Visitor::new("Shawn", "Hello Shawn, enjoy your treehouse!", VisitorAction::Accept, 32),
+        Visitor::new("Bert", "Hey Bernt Krishna, I like your stand up.", VisitorAction::AcceptWithNote{note: String::from("Severe alcoholic")}, 40),
+        Visitor::new("Tom", "Tom, get out.", VisitorAction::Refuse, 40),
     ];
 
-    for visitor in &visitor_list {
-        if visitor.name == name {
-            allow_them_in = true;
+    loop {
+        println!("Hello, what's your name? (Leave empty and press ENTER to quit)");
+        let name = what_is_your_name();
+    
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+    
+        if let Some(visitor) = known_visitor {
+             visitor.greet_visitor();
+        } else {
+            if name.is_empty() {
+                break;
+            } 
+
+            println!("{} is not on the visitor list.", name);
+            visitor_list.push(Visitor::new(&name, "New friend"));
         }
     }
-    if allow_them_in {
-        println!("Welcome, {}", &name);
-    } else {
-        println!("You are not on the guest list.");
-    }
+    println!("The final list of visitors:");
+    println!("{:#?}", visitor_list);
 }
-
 
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
